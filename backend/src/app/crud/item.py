e7132b15ecc8a352d66eaee111a8ce614afcebaf
rin-gil <91410075+rin-gil@users.noreply.github.com>
2025-06-29 12:57:01 +0300

    refactor: item.py

--- backend/src/app/crud/item.py
+++ backend/src/app/crud/item.py
@@ -1,9 +1,14 @@
 """Module for User CRUD operations for Item model."""
 
+from typing import Any, Type
 from uuid import UUID
 
+from sqlalchemy import Select
+from sqlalchemy.engine import TupleResult
+from sqlmodel import func, select
+
 from app.crud.base import BaseCRUD
-from app.models import Item, ItemCreate
+from app.models import Item, ItemCreate, ItemUpdate
 
 __all__: tuple[str] = ("ItemCRUD",)
 
@@ -11,16 +16,86 @@ __all__: tuple[str] = ("ItemCRUD",)
 class ItemCRUD(BaseCRUD):
     """CRUD operations for Item model."""
 
-    async def create(self, item_in: ItemCreate, owner_id: UUID) -> Item:
+    async def create_with_owner(self, *, item_in: ItemCreate, owner_id: UUID) -> Item:
         """
-        Create a new item in the database.
+        Creates a new item and assigns it to an owner.
 
-        :param item_in: Item creation data
-        :param owner_id: UUID of the item's owner
-        :return: Created Item object
+        :param item_in: Data required to create a new item.
+        :param owner_id: UUID of the user who will own the item.
+        :return: The created Item object with the owner assigned.
         """
         db_item: Item = Item.model_validate(obj=item_in, update={"owner_id": owner_id})
-        self._session.add(db_item)
+        self._session.add(instance=db_item)
         await self._session.commit()
-        await self._session.refresh(db_item)
+        await self._session.refresh(instance=db_item)
+        return db_item
+
+    async def get(self, item_id: UUID) -> Item | None:
+        """
+        Retrieve an item by their ID.
+
+        :param item_id: The UUID of the item.
+        :return: Item object or None if not found.
+        """
+        return await self._session.get(entity=Item, ident=(item_id,))
+
+    async def get_multi(self, *, skip: int = 0, limit: int = 100) -> tuple[list[Item], int]:
+        """
+        Retrieve multiple items from the database (for superusers).
+
+        :param skip: The number of items to skip from the start of the query.
+        :param limit: The maximum number of items to return.
+        :return: A tuple containing a list of items and the total count of items.
+        """
+        count_statement: Select[tuple[int]] = select(func.count()).select_from(Item)
+        count: int = (await self._session.exec(statement=count_statement)).scalar_one()
+        statement: Select[tuple[Item]] = select(Item).offset(offset=skip).limit(limit=limit)
+        result: TupleResult = await self._session.exec(statement=statement)
+        items: list[Item] = [item for item, in result.all()]
+        return items, count
+
+    async def get_multi_by_owner(self, *, owner_id: UUID, skip: int = 0, limit: int = 100) -> tuple[list[Item], int]:
+        """
+        Retrieve multiple items from the database (for regular users).
+
+        :param owner_id: The UUID of the user who owns the items.
+        :param skip: The number of items to skip from the start of the query.
+        :param limit: The maximum number of items to return.
+        :return: A tuple containing a list of items and the total count of items.
+        """
+        count_statement: Select[tuple[int]] = select(func.count()).select_from(Item).filter_by(owner_id=owner_id)
+        count: int = (await self._session.exec(statement=count_statement)).scalar_one()
+        statement: Select[tuple[Item]] = (
+            select(Item).filter_by(owner_id=owner_id).offset(offset=skip).limit(limit=limit)
+        )
+        result: TupleResult = await self._session.exec(statement=statement)
+        items: list[Item] = [item for item, in result.all()]
+        return items, count
+
+    async def update(self, *, db_item: Item, item_in: ItemUpdate) -> Item:
+        """
+        Updates an existing item.
+
+        :param db_item: Item object to update
+        :param item_in: Item update data
+        :return: Updated Item object.
+        """
+        update_dict: dict[str, Any] = item_in.model_dump(exclude_unset=True)
+        db_item.sqlmodel_update(obj=update_dict)
+        self._session.add(instance=db_item)
+        await self._session.commit()
+        await self._session.refresh(instance=db_item)
+        return db_item
+
+    async def remove(self, *, item_id: UUID) -> Item | None:
+        """
+        Deletes an item from the database.
+
+        :param item_id: The UUID of the item to delete.
+        :return: The deleted item, or None if no item was found.
+        """
+        db_item: Type[Item] | None = await self._session.get(entity=Item, ident=(item_id,))
+        if db_item:
+            await self._session.delete(instance=db_item)
+            await self._session.commit()
         return db_item
--- tests/backend/app/crud/test_item.py
+++ tests/backend/app/crud/test_item.py
@@ -1,38 +1,50 @@
 """Unit tests for backend/src/app/crud/item.py"""
 
-from unittest.mock import AsyncMock, MagicMock
+from unittest.mock import ANY, AsyncMock, MagicMock
 from uuid import UUID, uuid4
 
 import pytest
 from sqlmodel.ext.asyncio.session import AsyncSession
 
 from app.crud.item import ItemCRUD
-from app.models import Item, ItemCreate
+from app.models import Item, ItemCreate, ItemUpdate
 
 __all__: tuple = ()
 
 
-@pytest.mark.asyncio
-async def test_item_crud_create() -> None:
+@pytest.fixture
+def session_mock() -> AsyncSession:
     """
-    Test the create method of ItemCRUD.
+    Fixture to provide a mocked AsyncSession.
 
+    :return: Mocked AsyncSession object.
+    """
+    return AsyncMock(spec=AsyncSession)
+
+
+# noinspection PyUnresolvedReferences
+@pytest.mark.asyncio
+async def test_item_crud_create_with_owner(session_mock: AsyncSession) -> None:
+    """
+    Test the create_with_owner method of ItemCRUD.
+
+    :param session_mock: Mocked AsyncSession object.
     :return: None
     """
-    session_mock: AsyncMock = AsyncMock(spec=AsyncSession)
     item_crud: ItemCRUD = ItemCRUD(session=session_mock)
     item_in: ItemCreate = ItemCreate(title="Test Item", description="Test Description")
     owner_id: UUID = uuid4()
     db_item: Item = Item(title="Test Item", description="Test Description", owner_id=owner_id)
     Item.model_validate = MagicMock(return_value=db_item)
-    result: Item = await item_crud.create(item_in=item_in, owner_id=owner_id)
+    result: Item = await item_crud.create_with_owner(item_in=item_in, owner_id=owner_id)
     Item.model_validate.assert_called_once_with(obj=item_in, update={"owner_id": owner_id})
-    session_mock.add.assert_called_once_with(db_item)
+    session_mock.add.assert_called_once_with(instance=db_item)
     session_mock.commit.assert_called_once()
-    session_mock.refresh.assert_called_once_with(db_item)
+    session_mock.refresh.assert_called_once_with(instance=db_item)
     assert result == db_item, "Returned item should match the created item"
 
 
+# noinspection PyUnresolvedReferences
 @pytest.mark.asyncio
 @pytest.mark.parametrize(
     "item_title, item_description, owner_id",
@@ -40,24 +52,189 @@ async def test_item_crud_create() -> None:
     ids=["full_data", "no_description", "different_data"],
 )
 async def test_item_crud_create_with_different_inputs(
-    item_title: str, item_description: str | None, owner_id: UUID
+    session_mock: AsyncSession, item_title: str, item_description: str | None, owner_id: UUID
 ) -> None:
     """
-    Test the create method of ItemCRUD with different input parameters.
+    Test the create_with_owner method of ItemCRUD with different input parameters.
 
+    :param session_mock: Mocked AsyncSession object.
     :param item_title: Title of the item to create.
     :param item_description: Description of the item to create (optional).
     :param owner_id: UUID of the item's owner.
     :return: None
     """
-    session_mock: AsyncMock = AsyncMock(spec=AsyncSession)
     item_crud: ItemCRUD = ItemCRUD(session=session_mock)
     item_in: ItemCreate = ItemCreate(title=item_title, description=item_description)
     db_item: Item = Item(title=item_title, description=item_description, owner_id=owner_id)
     Item.model_validate = MagicMock(return_value=db_item)
-    result: Item = await item_crud.create(item_in=item_in, owner_id=owner_id)
+    result: Item = await item_crud.create_with_owner(item_in=item_in, owner_id=owner_id)
     Item.model_validate.assert_called_once_with(obj=item_in, update={"owner_id": owner_id})
-    session_mock.add.assert_called_once_with(db_item)
+    session_mock.add.assert_called_once_with(instance=db_item)
     session_mock.commit.assert_called_once()
-    session_mock.refresh.assert_called_once_with(db_item)
+    session_mock.refresh.assert_called_once_with(instance=db_item)
     assert result == db_item, f"Returned item should match the created item with title {item_title}"
+
+
+# noinspection PyUnresolvedReferences
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    "item_exists, item_id", [(True, uuid4()), (False, uuid4())], ids=["item_exists", "item_not_found"]
+)
+async def test_item_crud_get(session_mock: AsyncSession, item_exists: bool, item_id: UUID) -> None:
+    """
+    Test the get method of ItemCRUD.
+
+    :param session_mock: Mocked AsyncSession object.
+    :param item_exists: Whether the item exists in the database.
+    :param item_id: UUID of the item to retrieve.
+    :return: None
+    """
+    item_crud: ItemCRUD = ItemCRUD(session=session_mock)
+    db_item: Item | None = (
+        Item(title="Test Item", description="Test Description", owner_id=uuid4()) if item_exists else None
+    )
+    session_mock.get.return_value = db_item
+    result: Item | None = await item_crud.get(item_id=item_id)
+    session_mock.get.assert_called_once_with(entity=Item, ident=(item_id,))
+    assert result == db_item, f"Expected {'item' if item_exists else 'None'}, got {result}"
+
+
+# noinspection PyUnresolvedReferences
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    "skip, limit, items_count, expected_items",
+    [
+        (0, 100, 2, [Item(title="Item1", owner_id=uuid4()), Item(title="Item2", owner_id=uuid4())]),
+        (1, 1, 2, [Item(title="Item2", owner_id=uuid4())]),
+        (0, 100, 0, []),
+    ],
+    ids=["default_pagination", "skip_and_limit", "empty_result"],
+)
+async def test_item_crud_get_multi(
+    session_mock: AsyncSession, skip: int, limit: int, items_count: int, expected_items: list[Item]
+) -> None:
+    """
+    Test the get_multi method of ItemCRUD.
+
+    :param session_mock: Mocked AsyncSession object.
+    :param skip: Number of items to skip.
+    :param limit: Maximum number of items to return.
+    :param items_count: Total number of items in the database.
+    :param expected_items: Expected list of items to be returned.
+    :return: None
+    """
+    item_crud: ItemCRUD = ItemCRUD(session=session_mock)
+    session_mock.exec.side_effect = [
+        MagicMock(scalar_one=MagicMock(return_value=items_count)),
+        MagicMock(all=MagicMock(return_value=[(item,) for item in expected_items])),
+    ]
+    result_items, result_count = await item_crud.get_multi(skip=skip, limit=limit)
+    assert session_mock.exec.call_count == 2
+    session_mock.exec.assert_any_call(statement=ANY)
+    session_mock.exec.assert_any_call(statement=ANY)
+    assert result_items == expected_items, f"Expected items {expected_items}, got {result_items}"
+    assert result_count == items_count, f"Expected count {items_count}, got {result_count}"
+
+
+# noinspection PyUnresolvedReferences
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    "skip, limit, items_count, expected_items, owner_id",
+    [
+        (0, 100, 2, [Item(title="Item1", owner_id=uuid4()), Item(title="Item2", owner_id=uuid4())], uuid4()),
+        (1, 1, 2, [Item(title="Item2", owner_id=uuid4())], uuid4()),
+        (0, 100, 0, [], uuid4()),
+    ],
+    ids=["default_pagination", "skip_and_limit", "empty_result"],
+)
+async def test_item_crud_get_multi_by_owner(
+    session_mock: AsyncSession, skip: int, limit: int, items_count: int, expected_items: list[Item], owner_id: UUID
+) -> None:
+    """
+    Test the get_multi_by_owner method of ItemCRUD.
+
+    :param session_mock: Mocked AsyncSession object.
+    :param mocker: Pytest mocker fixture.
+    :param skip: Number of items to skip.
+    :param limit: Maximum number of items to return.
+    :param items_count: Total number of items for the owner.
+    :param expected_items: Expected list of items to be returned.
+    :param owner_id: UUID of the item's owner.
+    :return: None
+    """
+    item_crud: ItemCRUD = ItemCRUD(session=session_mock)
+    session_mock.exec.side_effect = [
+        MagicMock(scalar_one=MagicMock(return_value=items_count)),
+        MagicMock(all=MagicMock(return_value=[(item,) for item in expected_items])),
+    ]
+    result_items, result_count = await item_crud.get_multi_by_owner(owner_id=owner_id, skip=skip, limit=limit)
+    assert session_mock.exec.call_count == 2
+    session_mock.exec.assert_any_call(statement=ANY)
+    session_mock.exec.assert_any_call(statement=ANY)
+    assert result_items == expected_items, f"Expected items {expected_items}, got {result_items}"
+    assert result_count == items_count, f"Expected count {items_count}, got {result_count}"
+
+
+# noinspection PyUnresolvedReferences
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    "update_data, expected_update",
+    [
+        (
+            ItemUpdate(title="Updated Title", description="Updated Description"),
+            {"title": "Updated Title", "description": "Updated Description"},
+        ),
+        (ItemUpdate(title="Updated Title"), {"title": "Updated Title"}),
+        (ItemUpdate(description="Updated Description"), {"description": "Updated Description"}),
+    ],
+    ids=["full_update", "title_only", "description_only"],
+)
+async def test_item_crud_update(
+    session_mock: AsyncSession, update_data: ItemUpdate, expected_update: dict[str, str]
+) -> None:
+    """
+    Test the update method of ItemCRUD.
+
+    :param session_mock: Mocked AsyncSession object.
+    :param update_data: ItemUpdate object with data to update.
+    :param expected_update: Expected dictionary of updated fields.
+    :return: None
+    """
+    item_crud: ItemCRUD = ItemCRUD(session=session_mock)
+    db_item: Item = MagicMock(spec=Item)
+    result: Item = await item_crud.update(db_item=db_item, item_in=update_data)
+    db_item.sqlmodel_update.assert_called_once_with(obj=expected_update)
+    session_mock.add.assert_called_once_with(instance=db_item)
+    session_mock.commit.assert_called_once()
+    session_mock.refresh.assert_called_once_with(instance=db_item)
+    assert result is db_item, "Returned item should match the updated item"
+
+
+# noinspection PyUnresolvedReferences
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    "item_exists, item_id", [(True, uuid4()), (False, uuid4())], ids=["item_exists", "item_not_found"]
+)
+async def test_item_crud_remove(session_mock: AsyncSession, item_exists: bool, item_id: UUID) -> None:
+    """
+    Test the remove method of ItemCRUD.
+
+    :param session_mock: Mocked AsyncSession object.
+    :param item_exists: Whether the item exists in the database.
+    :param item_id: UUID of the item to delete.
+    :return: None
+    """
+    item_crud: ItemCRUD = ItemCRUD(session=session_mock)
+    db_item: Item | None = (
+        Item(title="Test Item", description="Test Description", owner_id=uuid4()) if item_exists else None
+    )
+    session_mock.get.return_value = db_item
+    result: Item | None = await item_crud.remove(item_id=item_id)
+    session_mock.get.assert_called_once_with(entity=Item, ident=(item_id,))
+    if item_exists:
+        session_mock.delete.assert_called_once_with(instance=db_item)
+        session_mock.commit.assert_called_once()
+    else:
+        session_mock.delete.assert_not_called()
+        session_mock.commit.assert_not_called()
+    assert result == db_item, f"Expected {'item' if item_exists else 'None'}, got {result}"
