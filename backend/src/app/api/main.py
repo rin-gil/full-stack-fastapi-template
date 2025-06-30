f91a82973cc0ac9f3c54703f12c0d94cce65fb57
rin-gil <91410075+rin-gil@users.noreply.github.com>
2025-06-30 08:04:33 +0300

    refactor: private.py

--- backend/src/app/api/main.py
+++ backend/src/app/api/main.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 
-from app.api.routes import private, items, login, users, utils
+from app.api.routes import items, login, users, utils
+from app.api.routes.private import private_router
 from app.core.config import get_settings, Settings
 
 settings: Settings = get_settings()
@@ -13,4 +14,4 @@ api_router.include_router(items.router)
 
 
 if settings.ENVIRONMENT == "local":
-    api_router.include_router(private.router)
+    api_router.include_router(router=private_router, prefix="/private", tags=["private"])
