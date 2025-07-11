d76d8fe8c7d732e30c7bae07d5f0f8a772bc1963
rin-gil <91410075+rin-gil@users.noreply.github.com>
2025-07-11 15:28:38 +0300

    style: change style

--- frontend/src/components/Common/ItemActionsMenu.tsx
+++ frontend/src/components/Common/ItemActionsMenu.tsx
@@ -1,23 +1,58 @@
-import { IconButton } from "@chakra-ui/react"
-import { BsThreeDotsVertical } from "react-icons/bs"
-import { MenuContent, MenuRoot, MenuTrigger } from "../ui/menu"
+/**
+ * @file Defines a menu for item actions (edit/delete).
+ * @description Renders a menu with edit and delete options for an item.
+ * @module ItemActionsMenu
+ */
+
+"use client"
 
 import type { ItemPublic } from "@/client"
+import { IconButton } from "@chakra-ui/react"
+import type * as React from "react"
+import type { FC } from "react"
+import { BsThreeDotsVertical } from "react-icons/bs"
 import DeleteItem from "../Items/DeleteItem"
 import EditItem from "../Items/EditItem"
+import { MenuContent, MenuRoot, MenuTrigger } from "../ui/menu"
 
+// region Type Aliases
+
+/**
+ * Props for the ItemActionsMenu component.
+ * @interface ItemActionsMenuProps
+ */
 interface ItemActionsMenuProps {
+  /** The item to perform actions on. */
   item: ItemPublic
 }
 
-export const ItemActionsMenu = ({ item }: ItemActionsMenuProps) => {
+/**
+ * Type alias for the ItemActionsMenu component.
+ * @type {ItemActionsMenuComponent}
+ */
+type ItemActionsMenuComponent = FC<ItemActionsMenuProps>
+
+// endregion
+
+// region Main Code
+
+/**
+ * ItemActionsMenu component for rendering a menu with edit and delete actions.
+ * @param {ItemActionsMenuProps} props - Props for the component.
+ * @returns {React.ReactElement} The rendered ItemActionsMenu component.
+ */
+export const ItemActionsMenu: ItemActionsMenuComponent = function ItemActionsMenu({
+  item,
+}: ItemActionsMenuProps): React.ReactElement {
+  const triggerButton: React.ReactElement = (
+    <IconButton variant="ghost" color="inherit">
+      <BsThreeDotsVertical />
+    </IconButton>
+  )
+
   return (
     <MenuRoot>
-      <MenuTrigger asChild>
-        <IconButton variant="ghost" color="inherit">
-          <BsThreeDotsVertical />
-        </IconButton>
-      </MenuTrigger>
+      <MenuTrigger asChild>{triggerButton}</MenuTrigger>
       <MenuContent>
         <EditItem item={item} />
         <DeleteItem id={item.id} />
@@ -25,3 +60,11 @@ export const ItemActionsMenu = ({ item }: ItemActionsMenuProps) => {
     </MenuRoot>
   )
 }
+
+// endregion
+
+// region Optional Declarations
+
+ItemActionsMenu.displayName = "ItemActionsMenu"
+
+// endregion
--- frontend/src/components/Common/UserActionsMenu.tsx
+++ frontend/src/components/Common/UserActionsMenu.tsx
@@ -1,24 +1,61 @@
-import { IconButton } from "@chakra-ui/react"
-import { BsThreeDotsVertical } from "react-icons/bs"
-import { MenuContent, MenuRoot, MenuTrigger } from "../ui/menu"
+/**
+ * @file Defines a menu for user actions (edit/delete).
+ * @description Renders a menu with edit and delete options for a user.
+ * @module UserActionsMenu
+ */
+
+"use client"
 
 import type { UserPublic } from "@/client"
+import { IconButton } from "@chakra-ui/react"
+import type * as React from "react"
+import type { FC } from "react"
+import { BsThreeDotsVertical } from "react-icons/bs"
 import DeleteUser from "../Admin/DeleteUser"
 import EditUser from "../Admin/EditUser"
+import { MenuContent, MenuRoot, MenuTrigger } from "../ui/menu"
 
+// region Type Aliases
+
+/**
+ * Props for the UserActionsMenu component.
+ * @interface UserActionsMenuProps
+ */
 interface UserActionsMenuProps {
+  /** The user to perform actions on. */
   user: UserPublic
+  /** Whether the menu trigger is disabled. */
   disabled?: boolean
 }
 
-export const UserActionsMenu = ({ user, disabled }: UserActionsMenuProps) => {
+/**
+ * Type alias for the UserActionsMenu component.
+ * @type {UserActionsMenuComponent}
+ */
+type UserActionsMenuComponent = FC<UserActionsMenuProps>
+
+// endregion
+
+// region Main Code
+
+/**
+ * UserActionsMenu component for rendering a menu with edit and delete actions for a user.
+ * @param {UserActionsMenuProps} props - Props for the component.
+ * @returns {React.ReactElement} The rendered UserActionsMenu component.
+ */
+export const UserActionsMenu: UserActionsMenuComponent = function UserActionsMenu({
+  user,
+  disabled,
+}: UserActionsMenuProps): React.ReactElement {
+  const triggerButton: React.ReactElement = (
+    <IconButton variant="ghost" color="inherit" disabled={disabled}>
+      <BsThreeDotsVertical />
+    </IconButton>
+  )
+
   return (
     <MenuRoot>
-      <MenuTrigger asChild>
-        <IconButton variant="ghost" color="inherit" disabled={disabled}>
-          <BsThreeDotsVertical />
-        </IconButton>
-      </MenuTrigger>
+      <MenuTrigger asChild>{triggerButton}</MenuTrigger>
       <MenuContent>
         <EditUser user={user} />
         <DeleteUser id={user.id} />
@@ -26,3 +63,11 @@ export const UserActionsMenu = ({ user, disabled }: UserActionsMenuProps) => {
     </MenuRoot>
   )
 }
+
+// endregion
+
+// region Optional Declarations
+
+UserActionsMenu.displayName = "UserActionsMenu"
+
+// endregion
